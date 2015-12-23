b338c91e7371125751c92324e2b2ff2d3718abb4
bimarvolo <vinh2304@gmail.com>
2015-12-23 10:42:35 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Phones
+st
---
+++ app.js
@@ -0,0 +1,31 @@
+// dfasf
+// fdasf
+
+
+myApp = angular.module('myApp', []);
+
+myApp.controller("Controller1", function($scope, $http){
+  $http.get('/phones/phones.json', {}).then(function (result){
+    $scope.phones = result.data;
+  });
+    $scope.message = 'Chào mừng các bạn đến với freetuts.net';
+});
+
+myApp.controller("Controller2", function($scope){
+    $scope.message = 'Học lập trình online freetuts.net';
+});
+
+/*var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+app.use(express.static('phones'));
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+*/
